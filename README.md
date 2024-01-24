7521414e72285908edb9e34d37f11df01f8fcec9
Nilesh Mali <nilesh@troplabs.com>
2024-01-24 16:46:38 +0530

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# vscode-utils
+
+Utilities for Visual Studio Code
+
+## Features
+
+- Base64 encode/decode
+
+## Commands
+
+- Base64 encode/decode
+  - `Base64 encode`: Encode selected text to Base64
+  - `Base64 decode`: Decode selected text from Base64
+
+## Keybindings
+
+- Mac
+  - `cmd+e cmd+e`: Base64 encode
+  - `cmd+e cmd+d`: Base64 decode
+
+- Windows/Linux
+  - `ctrl+e ctrl+e`: Base64 encode
+  - `ctrl+e ctrl+d`: Base64 decode
---
+++ src/extension.ts
@@ -0,0 +1,56 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode'
+import Window = vscode.window
+import Range = vscode.Range
+import Selection = vscode.Selection
+import TextDocument = vscode.TextDocument
+import TextEditor = vscode.TextEditor
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vscode-utils" is now active!')
+
+  const encode = vscode.commands.registerCommand('utils.base64Encode', () => {
+    const e = Window.activeTextEditor
+    const d = e?.document
+    const sel = e?.selections
+    base64Encode(e, d, sel)
+  })
+
+  const decode = vscode.commands.registerCommand('utils.base64Decode', () => {
+    const e = Window.activeTextEditor
+    const d = e?.document
+    const sel = e?.selections
+    base64Decode(e, d, sel)
+  })
+
+  context.subscriptions.push(encode)
+  context.subscriptions.push(decode)
+}
+
+function base64Encode(e?: TextEditor, d?: TextDocument, sel?: readonly Selection[]) {
+  e?.edit(edit => {
+    for (const s of sel ?? []) {
+      const txt = d?.getText(new Range(s.start, s.end))
+      const b: Buffer = Buffer.from(txt ?? '')
+      edit.replace(s, b.toString('base64'))
+    }
+  })
+}
+
+function base64Decode(e?: TextEditor, d?: TextDocument, sel?: readonly Selection[]) {
+  e?.edit(edit => {
+    for (const s of sel ?? []) {
+      const txt = d?.getText(new Range(s.start, s.end))
+      const b: Buffer = Buffer.from(txt ?? '', 'base64')
+      edit.replace(s, b.toString())
+    }
+  })
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
